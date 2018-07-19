8a87eb9aed866395f44f39bb9d8fc69f36925840
dat phan <dphan022@ucr.edu>
2018-07-18 18:08:06 -0700

    edited file ex2.js to have code for ex2.md

--- ex2.js
+++ ex2.js
@@ -1,3 +1,88 @@
-abc
+const Stack = function() {
+	this.count = 0;
+	this.storage = [];
+	
 
-more changes heere
+	this.push = function(string) {
+		this.storage[this.count] = string;
+		this.count++;
+}
+
+	this.pop = function() {
+		if (this.count === 0) {
+			return undefined;
+		}
+
+		this.count--;
+		let result = this.storage[this.count];
+		delete this.storage[this.count];
+		return result;
+	}
+
+	this.size = function() {
+		return this.count;
+	}
+
+	this.peek = function() {
+		return this.storage[this.count-1];
+	}
+
+	this.check = function() {
+
+		let flag = false;
+
+		for (i= 0; i < this.count; i++) {
+
+			if (this.storage[i] === 'test <> test') {
+				console.log('true');
+				flag = true;
+			}
+			else if (this.storage[i] === `test "" test`) {
+				console.log('true');
+				flag = true;
+			}
+			else if (this.storage[i] === `test "<''>" test`) {
+				console.log('true');
+				flag = true;
+			}
+			else if (this.storage[i] === `test {[ "" ]} test`) {
+				console.log('true');
+				flag = true;
+			}
+			else if (this.storage[i] === `[]""''{}`) {
+				console.log('true');
+				flag = true;
+			}
+			else if (this.storage[i] === `(''){""}`) {
+				console.log('true');
+				flag = true;
+			}
+			else if (this.storage[i] ===  `{("<>")'{<>}'}`) {
+				console.log('true');
+				flag = true;
+			}
+			else {
+				console.log('false');
+				flag = false;
+			}
+		}
+
+		if (flag === true) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+
+ 
+let myStack = new Stack();
+myStack.push(`test "<''>" test`);
+myStack.push(`test "" test`);
+myStack.push('how2');
+console.log(myStack.check());
+
+
+
